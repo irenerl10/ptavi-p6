0b70a5c86ba0c5cc109c252fff85439e3c895bd2
irenerl10 <irene17a@gmail.com>
2018-11-20 13:20:03 +0100

    en proceso

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    METHODS = ['INVITE', 'BYE', 'ACK']
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
@@ -27,15 +28,29 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 METHOD = line.decode('utf-8').split()[0]
             except:
                 pass
-            METHODS = ['INVITE', 'BYE', 'ACK']
-            print('x', METHOD, METHODS)
-
+            if METHOD == self.METHODS[0]:
+                self.wfile.write(b"\nSIP/2.0 100 Trying\nSIP/2.0 180 Ringing\nSIP/2.0 200 OK")
+            elif METHOD == self.METHODS[1]:
+                self.wfile.write(b"SIP/2.0 200 OK \n")
+            elif METHOD == self.METHODS[2]:
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 <' + sys.argv[3]
+                os.system(aEjecutar)
+            if METHOD != self.METHODS:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed \n")
+            else:
+                self.wfile.write(b"SIP/2.0 400 Bad Request \n")
+           
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            else:
+                print('line.decode('utf-8')')
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
     print("Lanzando servidor UDP de eco...")
+	  if os.path.isfile(FICH_AUDIO):
+        print("Listening...")
+        serv.serve_forever()
     serv.serve_forever()
