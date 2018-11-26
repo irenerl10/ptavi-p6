2473715c61ae2773f5d21721306f60b89040befd
Irene Rodriguez Lopez <irolo@gamma25.aulas.gsyc.urjc.es>
2018-11-26 10:42:56 +0100

    arreglado

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ if len(sys.argv)<4:
 SERVER = sys.argv[1]
 PORT = sys.argv[2]
 FILE = sys.argv[3]
-print('x', FILE)
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
@@ -22,31 +22,27 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     METHODS = ['INVITE', 'BYE', 'ACK']
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            try:
-                METHOD = line.decode('utf-8').split()[0]
-            except:
-                pass
+            print('line: ', line.decode('utf-8'))
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+            METHOD = line.decode('utf-8').split()[0]
             if METHOD == self.METHODS[0]:
-                self.wfile.write(b"\nSIP/2.0 100 Trying\nSIP/2.0 180 Ringing\nSIP/2.0 200 OK\n")
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ringing\r\n\r\nSIP/2.0 200 OK\r\n\r\n")
             elif METHOD == self.METHODS[1]:
                 self.wfile.write(b"SIP/2.0 200 OK \n")
             elif METHOD == self.METHODS[2]:
                 aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 <' + sys.argv[3]
                 os.system(aEjecutar)
-            if METHOD != self.METHODS:
+            elif METHOD != self.METHODS:
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed \n")
             else:
                 self.wfile.write(b"SIP/2.0 400 Bad Request \n")
-           
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
-            else:
-                print(line.decode('utf-8'))
+
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
